35dcf24ba43364c03c6d447e75ecb69e33da9fb7
Johanna Åstrand <johanna@johannas-mbp.mynet>
2020-04-29 20:10:12 +0200

    finished project

--- code/src/reducers/nutritioninfo.js
+++ code/src/reducers/nutritioninfo.js
@@ -15,14 +15,12 @@ export const nutritionInfo = createSlice({
 
 
 export const fetchFood = (code) => {
-  // console.log(`Code: ${code}`)
   const FOOD_URL = `https://world.openfoodfacts.org/api/v0/product/${code}.json`;
   return (dispatch) => {
     dispatch(ui.actions.setLoading(true))
     fetch(FOOD_URL)
       .then((res) => res.json())
       .then((json) => {
-        console.log(json)
         dispatch(nutritionInfo.actions.setNutritionInfo(json))
         dispatch(ui.actions.setLoading(false))
       })
